b0f4f9f73708b4fedad0aaac820168055af9eb33
Mykhailo Lebediev <mykhailo.lebediev@blueacorn.com>
2019-03-13 17:54:44 +0200

    Examples page rename - 2

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 let Examples = (props) => {
     return (
         <div>
-            <h3>Examples component!</h3>
+            <h3>Examples</h3>
         </div>
     )
 }
