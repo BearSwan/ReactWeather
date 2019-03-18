cf78c285ddc1a92fec55527ad695ccec6b498f17
Mykhailo Lebediev <mykhailo.lebediev@blueacorn.com>
2019-03-18 12:54:24 +0200

    ErrorModal created

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+let ErrorModal = React.createClass({
+    getDefaultProps: function () {
+        return {
+            title: "Error"
+        }
+    },
+    propTypes: {
+        title: React.propTypes.string,
+        message: React.propTypes.string.isRequired
+    },
+    componentDidMount: function () {
+        let modal = new Foundation.Reveal($("#error-modal"));
+        modal.open();
+    },
+    render: function () {
+        let {title, message} = this.props;
+        return (
+            <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+                <h4 className="title">{title}</h4>
+                <p className="error-message">{message}}</p>
+                <button className="button hollow" data-close="">OK</button>
+            </div>
+        );
+    }
+});
+
+export default ErrorModal;
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,52 +1,63 @@
 import React from 'react';
 import WeatherForm from 'WeatherForm';
 import WeatherMessage from 'WeatherMessage';
-var openWeatherMap = require('openWeatherMap');
+import ErrorModal from 'ErrorModal';
+let openWeatherMap = require('openWeatherMap');
 
-var Weather = React.createClass({
+let Weather = React.createClass({
     getInitialState: function () {
-        return {
-            isLoading: false
-        }
+      return {
+        isLoading: false
+      }
     },
     handleSearch: function (location) {
-        let that = this;
-
-        this.setState({
-            isLoading: true
+      let that = this;
+  
+      this.setState({
+        isLoading: true,
+        errorMessage: undefined
+      });
+  
+      openWeatherMap.getTemp(location).then(function (temp) {
+        that.setState({
+          location: location,
+          temp: temp,
+          isLoading: false
         });
-
-        openWeatherMap.getTemp(location).then(function (temp) {
-            that.setState({
-                location: location,
-                temp: temp,
-                isLoading: false
-            })
-        }, function (errorMessage) {
-            that.setState({
-                isLoading: false
-            })
-            alert(errorMessage);
+      }, function (e) {
+        that.setState({
+          isLoading: false,
+          errorMessage: e.message
         });
+      });
     },
     render: function () {
-        var {isLoading, temp, location} = this.state;
-
-        function renderMessage() {
-            if (isLoading) {
-                return <h3 className="text-center">Fetching weather....</h3>
-            } else if (temp && location) {
-                return <WeatherMessage temp={temp} location={location}/>;
-            } 
+      var {isLoading, temp, location, errorMessage} = this.state;
+  
+      function renderMessage () {
+        if (isLoading) {
+          return <h3 className="text-center">Fetching weather...</h3>;
+        } else if (temp && location) {
+          return <WeatherMessage temp={temp} location={location}/>;
         }
-
-        return (
-            <div>
-                <h1 className="page-title text-center">Get weather</h1>
-                <WeatherForm onSearch={this.handleSearch}/>
-                {renderMessage()}
-            </div>
-        );
+      }
+  
+      function renderError () {
+        if (typeof errorMessage === 'string') {
+          return (
+            <ErrorModal message={errorMessage}/>
+          )
+        }
+      }
+  
+      return (
+        <div>
+          <h1 className="text-center">Get Weather</h1>
+          <WeatherForm onSearch={this.handleSearch}/>
+          {renderMessage()}
+          {renderError()}
+        </div>
+      )
     }
 });
 
